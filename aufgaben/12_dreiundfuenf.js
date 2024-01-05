e097b030d6b54a92b8934e9df5b003991f612908
donatht <107429042+donatht@users.noreply.github.com>
2024-01-05 09:40:00 +0000

    12 erfolgreich

--- aufgaben/12_dreiundfuenf.js
+++ aufgaben/12_dreiundfuenf.js
@@ -1,6 +1,12 @@
 function threeAndFive(border) {
    // code here
-   
+   let sum = 0
+   for (let i= 1; i<border; i++){
+       if ((i % 3 == 0) || (i % 5 == 0)){
+        sum +=i
+       }
+   }
+   return sum
 }
 
 /*
